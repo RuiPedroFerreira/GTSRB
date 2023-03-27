d8b7dc85a4661cf9932de5191b2d1b66dc956260
ruipedro <r.pedropassos21@gmail.com>
2023-03-27 16:57:53 +0100

    repeat upgrade count

--- create_csv.py
+++ create_csv.py
@@ -8,9 +8,10 @@ from random import uniform
 directory = "Dataset_Final_Augmentation/"
 output_csv_train = "train.csv"
 output_csv_test = "test.csv"
+output_csv_validation = "validation.csv"
 
 learning_set = 0.8
-# validation_set = 0.2
+validation_set = 0.1
 
 def get_sub_folders(directory):
     return [os.path.join(directory, f) for f in os.listdir(directory) if os.path.isdir(os.path.join(directory, f))]
@@ -25,7 +26,6 @@ def write_csv(csv_file, label, list_of_files):
 
 if __name__ == "__main__":
 
-
     subfolders = get_sub_folders(directory)
         # List all sub-folders in the parent director
     
@@ -34,14 +34,23 @@ if __name__ == "__main__":
         sub_sub_folders = get_sub_folders(name_fold)
 
         for folder in sub_sub_folders:
-            print(folder)
+            #print(folder)
             randvalue = random.uniform(0,1)
 
+            if randvalue < learning_set:
+                outputcsv = output_csv_train
+            elif learning_set < randvalue < learning_set + validation_set:
+                outputcsv = output_csv_validation
+            else:
+                outputcsv = output_csv_test
+
+            '''
             if (randvalue < learning_set):
                 outputcsv = output_csv_train
             else:
                 outputcsv = output_csv_test
-        
+            '''
+            
             sub_sub_folders_files = get_files(folder)
             #print(sub_sub_folders_files, outputcsv)
             write_csv(outputcsv, label, sub_sub_folders_files)
--- repeat_name.py
+++ repeat_name.py
@@ -3,13 +3,14 @@ import os
 import shutil
 
 
-directory = '/Users/ruipedro/Desktop/Dataset_Final_v2'
+directory = 'Dataset_Final_Augmentation/'
 
 subfolders = [os.path.join(directory, f) for f in os.listdir(directory) if os.path.isdir(os.path.join(directory, f))]
 
 aux_name = None
 
 repeat_list = []
+
 for subfolder in subfolders:
     for file in os.listdir(subfolder):
         if file == aux_name:
